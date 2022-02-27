4e7a001751cfd09fb71de24beb9b2c411188adaf
Limfin <victor010389@yandex.ru>
2022-02-27 16:50:02 +0400

    Оформление заказа. Использование TimestampBehavior для автоматического обновления даты заказы при его изменении(updated_at) или создании(created_at). Настройка очистки корзины после заказа.

--- views/cart/view.php
+++ views/cart/view.php
@@ -6,6 +6,20 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="container">
+	<?php if (Yii::$app->session->hasFlash('success')) : ?>
+		<div class="alert alert-success alert-dismissible" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<?= Yii::$app->session->getFlash('success'); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if (Yii::$app->session->hasFlash('error')) : ?>
+		<div class="alert alert-danger alert-dismissible" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<?= Yii::$app->session->getFlash('error'); ?>
+		</div>
+	<?php endif; ?>
+
 	<?php if (!empty($session['cart'])) : ?>
 		<h2>Корзина полна</h2>
 		<section id="cart_items">
